0289445015ea009d9c096b37d0c372af3ad00507
Muteeb Ur Rehman <139336141+muteeburrehman@users.noreply.github.com>
2023-07-12 17:27:02 +0500

    HTTP Components

---
+++ src/HTTP Components/components/ChildComp.vue
@@ -0,0 +1,35 @@
+<template>
+<div>Child Component</div>
+</template>
+<script>
+export default {
+  name: 'ChildComp',
+  beforeCreate() {
+    console.log('ChildComp beforeCreate()')
+  },
+  created() {
+    console.log('ChildComp created()')
+  },
+  beforeMount() {
+    console.log('ChildComp beforeMount()')
+  },
+  mounted() {
+    console.log('ChildComp mounted()')
+  },
+  beforeUpdate() {
+    console.log('ChildComp beforeUpdate()')
+  },
+  updated() {
+    console.log('ChildComp updated()')
+  },
+  beforeUnmount() {
+    console.log('ChildComp beforeUnmount()')
+  },
+  unmounted() {
+    console.log('ChildComp unmounted()')
+  },
+}
+</script>
+<style scoped>
+
+</style>
