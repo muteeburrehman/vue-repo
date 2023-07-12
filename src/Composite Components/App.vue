45369473a1374afcbb9ab4ae8afcafe9d5c8994c
Muteeb Ur Rehman <139336141+muteeburrehman@users.noreply.github.com>
2023-07-12 17:19:32 +0500

    Composite API Components

---
+++ src/Composite Components/App.vue
@@ -0,0 +1,66 @@
+<template>
+  <DataComp/>
+  <DemoOne/>
+  <DemoTwo/>
+  <MethodS/>
+  <VModel/>
+  <ComputedComp/>
+  <WatchComp/>
+  <ProvideInject/>
+  <ChildA/>
+  <ChildB/>
+  <ChildC/>
+  <TemplateRef/>
+  <PersonComp/>
+  <PersonGreeting/>
+</template>
+
+<script>
+import DataComp from "@/components/DataComp.vue";
+import DemoOne from "@/components/DemoOne.vue";
+import DemoTwo from "@/assets/DemoTwo.vue";
+import MethodS from "@/components/MethodS.vue";
+import VModel from "@/components/VModel.vue";
+import ComputedComp from "@/components/ComputedComp.vue";
+import WatchComp from "@/components/WatchComp.vue";
+import ProvideInject from "@/components/ProvideInject.vue";
+import ChildA from "@/components/ChildA.vue";
+import ChildB from "@/components/ChildB.vue";
+import ChildC from "@/components/ChildC.vue";
+import TemplateRef from "@/components/TemplateRef.vue";
+import PersonComp from "@/components/PersonComp.vue";
+import PersonGreeting from "@/components/PersonGreeting.vue";
+
+
+export default {
+  name: 'App',
+  components: {
+
+    PersonGreeting,
+    PersonComp,
+    TemplateRef,
+    ChildC,
+    ChildB,
+    ChildA,
+    ProvideInject,
+    WatchComp,
+    ComputedComp,
+    VModel,
+    MethodS,
+    DataComp,
+    DemoOne,
+    DemoTwo,
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
