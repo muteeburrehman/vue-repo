281bddd4465debfc87338cb22ac4e52668ab02ef
Muteeb Ur Rehman <139336141+muteeburrehman@users.noreply.github.com>
2023-07-12 15:08:25 +0500

    Add files via upload

---
+++ src/App.vue
@@ -0,0 +1,35 @@
+<template>
+  <PostList/>
+  <CreatePost/>
+  <ParentComp/>
+  <TemplateRef/>
+</template>
+
+<script>
+import PostList from "@/components/PostList.vue";
+import CreatePost from "@/components/CreatePost.vue";
+import ParentComp from "@/components/ParentComp.vue";
+import TemplateRef from "@/components/TemplateRef.vue";
+export default {
+  name: 'App',
+  components: {
+    PostList,
+    CreatePost,
+    ParentComp,
+    TemplateRef,
+  },
+
+  }
+
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
